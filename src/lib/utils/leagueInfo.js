85239fc12b48ae269fc9e64c3539839bafafa1c3
Ryan Robinson <RyzoRobinson@gmail.com>
2024-09-11 08:38:28 +0100

    Update leagueInfo.js
    Adding photos to league.js

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -26,10 +26,10 @@ export const managers = [
        "tookOver": 2020, // (optional) used if a manager took over a team, delete this line or change to null otherwise
        "location": "Craigavon", // (optional)
        "bio": "Loves Wednesdays",
-       "photo": "/managers/name.jpg", // square ratio recommended (no larger than 500x500)
+       "photo": "/managers/ryzo.jpg", // square ratio recommended (no larger than 500x500)
        "fantasyStart": 2016, // (optional) when did the manager start playing fantasy football
        "favoriteTeam": "lv", // (optional) favorite NFL team, (follows convention: nyj, sea, mia, etc.) MUST BE LOWERCASE
-       "mode": "Win Now", // (optional) 'Win Now', 'Dynasty', or 'Rebuild' (anything else and you will need to add a new png to /static/ similar to the 'Rebuild.png' and 'Win Now.png' currently in there)
+       "mode": null, // (optional) 'Win Now', 'Dynasty', or 'Rebuild' (anything else and you will need to add a new png to /static/ similar to the 'Rebuild.png' and 'Win Now.png' currently in there)
        "rival": {
          name: "Josh McDaniels (Curse upon his family)", // Can be anything (usually your rival's name)
          link: 2, // manager array number within this array, or null to link back to all managers page
@@ -70,7 +70,7 @@ export const managers = [
        "tookOver": 2020, // (optional) used if a manager took over a team, delete this line or change to null otherwise
        "location": "London, East Ireland", // (optional)
        "bio": "after achieving all that he could achieve at Castlewellan under 14s, Seán turned his eye to coaching. Seán values grit above all traits in his players (Danny Woodhead, John Kuhn, Kyle Juszczyk come to mind), guys who are the first to arrive to the facility and last to leave. Despite being the Jim Kelly Buffalo Bills of the Bines Bowl, Seáns undying commitment to win with Ulster Says No Huddle has only grown stronger. Seán also takes his role as Bines Bowl International Series ambassador very seriously.",
-       "photo": "/managers/name.jpg", // square ratio recommended (no larger than 500x500)
+       "photo": "/managers/skeown.jpg", // square ratio recommended (no larger than 500x500)
        "fantasyStart": 2010, // (optional) when did the manager start playing fantasy football
        "favoriteTeam": "den", // (optional) favorite NFL team, (follows convention: nyj, sea, mia, etc.) MUST BE LOWERCASE
 	"mode": null, // (optional) 'Win Now', 'Dynasty', or 'Rebuild' (anything else and you will need to add a new png to /static/ similar to the 'Rebuild.png' and 'Win Now.png' currently in there)
@@ -92,7 +92,7 @@ export const managers = [
        "tookOver": null, // (optional) used if a manager took over a team, delete this line or change to null otherwise
        "location": "Belfast", // (optional)
        "bio": "I am a fun loving, laid back guy who doesn't care for any overly confrontational attitude or childish insults. I'm only here for the craic and my lack of fantasy success doesn't hurt or worry me at all.",
-       "photo": "/managers/name.jpg", // square ratio recommended (no larger than 500x500)
+       "photo": "/managers/eoin.jpg", // square ratio recommended (no larger than 500x500)
        "fantasyStart": null, // (optional) when did the manager start playing fantasy football
        "favoriteTeam": "no", // (optional) favorite NFL team, (follows convention: nyj, sea, mia, etc.) MUST BE LOWERCASE
 	   "mode": null, // (optional) 'Win Now', 'Dynasty', or 'Rebuild' (anything else and you will need to add a new png to /static/ similar to the 'Rebuild.png' and 'Win Now.png' currently in there)
@@ -114,7 +114,7 @@ export const managers = [
        "tookOver": 2020, // (optional) used if a manager took over a team, delete this line or change to null otherwise
        "location": "Belfast", // (optional)
        "bio": "Looking for a fourth playoff appearance and a first bines bowl in my fifth season as part of this great league. I have won an American football trophy (see photo), but I would trade it in a heartbeat for that sweet, sweet bines bowl.",
-       "photo": "/managers/name.jpg", // square ratio recommended (no larger than 500x500)
+       "photo": "/managers/colm.jpg", // square ratio recommended (no larger than 500x500)
        "fantasyStart": 2020, // (optional) when did the manager start playing fantasy football
        "favoriteTeam": "jax", // (optional) favorite NFL team, (follows convention: nyj, sea, mia, etc.) MUST BE LOWERCASE
     "mode": null, // (optional) 'Win Now', 'Dynasty', or 'Rebuild' (anything else and you will need to add a new png to /static/ similar to the 'Rebuild.png' and 'Win Now.png' currently in there)
@@ -135,7 +135,7 @@ export const managers = [
        "tookOver": 2021, // (optional) used if a manager took over a team, delete this line or change to null otherwise
        "location": "Kilress", // (optional)
        "bio": "6'3 and Gifted",
-       "photo": "/managers/name.jpg", // square ratio recommended (no larger than 500x500)
+       "photo": "/managers/conall.jpg", // square ratio recommended (no larger than 500x500)
        "fantasyStart": 2015, // (optional) when did the manager start playing fantasy football
        "favoriteTeam": "hou", // (optional) favorite NFL team, (follows convention: nyj, sea, mia, etc.) MUST BE LOWERCASE
     "mode": null, // (optional) 'Win Now', 'Dynasty', or 'Rebuild' (anything else and you will need to add a new png to /static/ similar to the 'Rebuild.png' and 'Win Now.png' currently in there)
@@ -157,7 +157,7 @@ export const managers = [
        "tookOver": 2018, // (optional) used if a manager took over a team, delete this line or change to null otherwise
        "location": "Derriaghy/Carndonagh", // (optional)
        "bio": "Some men are born into greatness, others are forged by their environments. Cathal Kelly is neither of those. At the ripe age of 23ish, Allah himself intervened in the life of Cathal Kelly, bestowing upon him the gift of superior fantasy football knowledge and a really nuanced culinary palate. Cathal led by example in the Bines Bowl's early years, lighting the way for those who came after him. Some would attribute the success of his successors to Cathal, but he doesn't know about all that. He's just happy to be able to play a silly little game with his friends.",
-       "photo": "/managers/name.jpg", // square ratio recommended (no larger than 500x500)
+       "photo": "/managers/cathal.jpg", // square ratio recommended (no larger than 500x500)
        "fantasyStart": 2018, // (optional) when did the manager start playing fantasy football
        "favoriteTeam": "lar", // (optional) favorite NFL team, (follows convention: nyj, sea, mia, etc.) MUST BE LOWERCASE
     "mode": null, // (optional) 'Win Now', 'Dynasty', or 'Rebuild' (anything else and you will need to add a new png to /static/ similar to the 'Rebuild.png' and 'Win Now.png' currently in there)
@@ -201,7 +201,7 @@ export const managers = [
        "tookOver": 2019, // (optional) used if a manager took over a team, delete this line or change to null otherwise
        "location": "Liverpool/Castlewellan", // (optional)
        "bio": "This young, often mustachiod man has the highest regular season win rate in Bines bowl history. Having never fallen below .500 in a season, he is a lock for the post season every year, but has never won a trophy. The true Dallas Cowboys of the Bines Bowl.",
-       "photo": "/managers/name.jpg", // square ratio recommended (no larger than 500x500)
+       "photo": "/managers/rory.jpg", // square ratio recommended (no larger than 500x500)
        "fantasyStart": 2019, // (optional) when did the manager start playing fantasy football
        "favoriteTeam": "dal", // (optional) favorite NFL team, (follows convention: nyj, sea, mia, etc.) MUST BE LOWERCASE
        "mode": null, // (optional) 'Win Now', 'Dynasty', or 'Rebuild' (anything else and you will need to add a new png to /static/ similar to the 'Rebuild.png' and 'Win Now.png' currently in there)
@@ -223,7 +223,7 @@ export const managers = [
        "tookOver": 2010, // (optional) used if a manager took over a team, delete this line or change to null otherwise
        "location": "Glorious Clonoe/Stranmillis", // (optional)
        "bio": "As a GM, I live for the excitement, I live for the fans. First year in the Fantasy Game was incredibly strong, just falling short of the Bines Bowl. Year 2, rather than stay the course in incrementally improve, we mix it up. We get punished. Severely. But that does naught more than fill the Tin Pot Organisation with a hunger that could only be sated by the greatest revival of the last 2000 years in. The resume speaks for itself. #IfYouKnowYourHistory",
-       "photo": "/managers/name.jpg", // square ratio recommended (no larger than 500x500)
+       "photo": "/managers/paul.jpg", // square ratio recommended (no larger than 500x500)
        "fantasyStart": 2020, // (optional) when did the manager start playing fantasy football
        "favoriteTeam": "nyg", // (optional) favorite NFL team, (follows convention: nyj, sea, mia, etc.) MUST BE LOWERCASE
     "mode": null, // (optional) 'Win Now', 'Dynasty', or 'Rebuild' (anything else and you will need to add a new png to /static/ similar to the 'Rebuild.png' and 'Win Now.png' currently in there)
@@ -245,7 +245,7 @@ export const managers = [
        "tookOver": 2019, // (optional) used if a manager took over a team, delete this line or change to null otherwise
        "location": "Clonoe/Belfast", // (optional)
        "bio": "Bine enthusiast. Full time hater. Enjoyer of all things Sicko. Never wrong, just early...",
-       "photo": "/managers/name.jpg", // square ratio recommended (no larger than 500x500)
+       "photo": "/managers/brian.jpg", // square ratio recommended (no larger than 500x500)
        "fantasyStart": 2019, // (optional) when did the manager start playing fantasy football
        "favoriteTeam": "bal", // (optional) favorite NFL team, (follows convention: nyj, sea, mia, etc.) MUST BE LOWERCASE
     "mode": null, // (optional) 'Win Now', 'Dynasty', or 'Rebuild' (anything else and you will need to add a new png to /static/ similar to the 'Rebuild.png' and 'Win Now.png' currently in there)
@@ -267,7 +267,7 @@ export const managers = [
        "tookOver": 2022, // (optional) used if a manager took over a team, delete this line or change to null otherwise
        "location": "London", // (optional)
        "bio": "Pretty Drunk",
-       "photo": "/managers/name.jpg", // square ratio recommended (no larger than 500x500)
+       "photo": "/managers/frank.jpg", // square ratio recommended (no larger than 500x500)
        "fantasyStart": 2022, // (optional) when did the manager start playing fantasy football
     //   "favoriteTeam": "bal", // (optional) favorite NFL team, (follows convention: nyj, sea, mia, etc.) MUST BE LOWERCASE
     "mode": null, // (optional) 'Win Now', 'Dynasty', or 'Rebuild' (anything else and you will need to add a new png to /static/ similar to the 'Rebuild.png' and 'Win Now.png' currently in there)
